2b08b601a856cc37071c3f52f43cff8dcb05c9af
Junho123a <kjhh808@naver.com>
2026-02-27 13:50:27 +0900

    initial commit

---
+++ var_card.py
@@ -0,0 +1,15 @@
+
+def var_card():
+    deck = 0
+    monstar_handtrap = 0#패트랩 수, 후공일 경우 상대 원핸드와 1대1교환 
+    monstar_onehand_normal = 0#일소 원핸드(1번밖에 사용불가)
+    monstar_onehand_special = 0#특소 원핸드(일소 불가)
+    monstar_mathmech_circular = 0 #서큘러급
+    monstar_onehand_ns = 0
+    monstar_twohand = 0#다른 카드와 조합시 승리
+    monstar_crush = 0
+
+    banmaxxc = 1
+    ash_blossom = 3
+    marucami_huwaros = 2
+    
